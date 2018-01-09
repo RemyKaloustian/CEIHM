e788db2b9b9e06485c6388d95d7114600f8ea93b
Rémy Kaloustian <remy.kaloustian@gmail.com>
2018-01-09 11:36:19 +0100

    Hiding the new address popup when valdating new address

--- js/address.js
+++ js/address.js
@@ -5,16 +5,26 @@ $(document).ready(function()
 
     $("#validate-address").click(validateAddress);
     $("#new-address").click(showNewAddress);
+    $("#validate-new-address").click(validateNewAddress);
 
     function validateAddress()
     {
         $("#address-popup").hide();
+        //TODO : Make stylish animations
     }
 
     function showNewAddress()
     {
         $("#address-popup").hide();
         $("#new-address-popup").show();
+
+        //TODO : Make stylish animations
+    }
+
+    function validateNewAddress()
+    {
+        $("#new-address-popup").hide();
+        //TODO : center the map on the new address
     }
 
 });
