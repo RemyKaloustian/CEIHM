2dac08e18686637263230ed49c5c25cbf0c1d4a4
Rémy Kaloustian <remy.kaloustian@gmail.com>
2018-01-09 15:08:22 +0100

    Work on the side display in progress : boom: :boom: :feelsgood: :rage2: :rage4: :goberserk:

---
+++ js/sidedisplay.js
@@ -0,0 +1,24 @@
+/*
+*@Purpose : Contains the management of the side display
+*@Authors:
+*Thomas Monzein
+*Anthonny Giroud
+*Kevin Duglué
+*Rémy Kaloustian
+*/
+
+$(document).ready(function()
+{
+   //$("#side-button").click(showSideDisplay);
+
+   function showSideDisplay()
+   {
+       //$("#side-display").css("marginLeft", "400px");
+
+       const newmargin = $(window).width() - $("#side-display").width();
+       console.log("NEWMARGIN = " + newmargin + $("#side-display").css("margin-left") + $("#side-display").width());
+       
+       $("#side-display").css("marginLeft", $(window).width() - $("#side-display").width());
+   }
+
+});
