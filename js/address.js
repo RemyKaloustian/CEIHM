4e8236f5f049ca1ef1e4020381061baee05f85c1
Rémy Kaloustian <remy.kaloustian@gmail.com>
2018-01-09 12:08:48 +0100

    Showing the fire hydrants in the address popup :triump: :boom: :zap: :rage4:

--- js/address.js
+++ js/address.js
@@ -1,15 +1,29 @@
 $(document).ready(function()
 {
-    let address = {};
-    address.fullName = "23 rue de la destruction des tétons";
-    $("#address-popup h4").text(address.fullName);
+    let _address = {};
+    _address.fullName = "23 rue de la destruction des tétons";
+    $("#address-popup h4").text(_address.fullName);
 
+    let _fireHydrants = [];
+    _fireHydrants.push("3 rue du charisme");
+    _fireHydrants.push("45 boulevard du communisme");
+    _fireHydrants.push("7272 allée des Quicksopes");
 
+    showFireHydrants();
 
     $("#validate-address").click(validateAddress);
     $("#new-address").click(showNewAddress);
     $("#validate-new-address").click(validateNewAddress);
     $("#cancel-new-address").click(cancelNewAddress);
+
+    function showFireHydrants()
+    {
+        for(var i = 0; i<_fireHydrants.length ; ++i)
+        {
+            $("#fire-hydrants").append("<p>" + _fireHydrants[i] +"</p>");
+
+        }
+    }
 
     function validateAddress()
     {
