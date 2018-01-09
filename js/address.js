1c65089cc1f45d4348b0c58268cc5afa595e310f
Rémy Kaloustian <remy.kaloustian@gmail.com>
2018-01-09 11:00:24 +0100

    Hiding popup when validating :sunglasses: :heartpulse:

--- js/address.js
+++ js/address.js
@@ -1,5 +1,13 @@
-$(document).ready(function(){
+$(document).ready(function()
+{
 
     console.log("Address done");
+
+    $("#validate-address").click(validateAddress);
+
+    function validateAddress()
+    {
+        $("#address-popup").hide();
+    }
 
 });
--- js/style.js
+++ js/style.js
@@ -1,4 +1,5 @@
-$(document).ready(function(){
+$(document).ready(function()
+{
     const marginTop =($( window ).height() - $(".popup-content").height())/2;
    $(".popup-content").css("marginTop", marginTop - 100);
 
