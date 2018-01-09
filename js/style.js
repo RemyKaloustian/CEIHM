15011b9fd9eaf65186cadf0300378e88c4019949
Rémy Kaloustian <remy.kaloustian@gmail.com>
2018-01-09 14:47:20 +0100

    Hiding the side display :boom: :boom: boom: :fire: :feelsgood:

--- js/style.js
+++ js/style.js
@@ -10,8 +10,12 @@
 $(document).ready(function()
 {
     $("#new-address-popup").hide();
+
     //Centering the popup content vertically
     const marginTop =($( window ).height() - $(".popup-content").height())/2;
     $(".popup-content").css("marginTop", marginTop - 100);
+
+    //Setting the side display properties
+    $("#side-display").css("marginLeft", $(window).width());
 
 });
