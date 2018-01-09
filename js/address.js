f2da1f63f413eba13fb44be7c9f93cfb96bae9a7
Rémy Kaloustian <remy.kaloustian@gmail.com>
2018-01-09 11:28:11 +0100

    Showing the new address popup when clicking new address :muscle: :boom: :horse_racing:

--- js/address.js
+++ js/address.js
@@ -4,10 +4,17 @@ $(document).ready(function()
     console.log("Address done");
 
     $("#validate-address").click(validateAddress);
+    $("#new-address").click(showNewAddress);
 
     function validateAddress()
     {
         $("#address-popup").hide();
+    }
+
+    function showNewAddress()
+    {
+        $("#address-popup").hide();
+        $("#new-address-popup").show();
     }
 
 });
--- js/style.js
+++ js/style.js
@@ -1,5 +1,6 @@
 $(document).ready(function()
 {
+    $("#new-address-popup").hide();
     const marginTop =($( window ).height() - $(".popup-content").height())/2;
    $(".popup-content").css("marginTop", marginTop - 100);
 
