06540fa969bceaee9cf089341130b9e013f0c4a9
Kevin Duglué <kevin.duglue@gmail.com>
2018-01-17 11:28:23 +0100

    InfoWindow + Partial autocomplete

--- js/address.js
+++ js/address.js
@@ -23,7 +23,9 @@ $(document).ready(function()
     $(".current-fire").click(function(event){
         setAddress($(event.target).text());
     });
-
+    var autocomplete;
+    console.log('coucou');
+    initAutocomplete();
     
 
     function showCurrentFires()
@@ -104,5 +106,38 @@ $(document).ready(function()
             }, 500, function() {
             // Animation complete.
           });
+    }
+
+    function initAutocomplete() {
+        
+        // Create the autocomplete object, restricting the search to geographical
+        // location types.
+        
+        var autocomplete = new google.maps.places.Autocomplete(
+        /** @type {!HTMLInputElement} */(document.getElementById('newAdress')),
+        {types: ['geocode']});
+        // When the user selects an address from the dropdown, populate the address
+        // fields in the form.
+        //autocomplete.addListener('place_changed', fillInAddress);
+    }
+
+    function fillInAddress() {
+        // Get the place details from the autocomplete object.
+        var place = autocomplete.getPlace();
+
+        for (var component in componentForm) {
+            document.getElementById(component).value = '';
+            document.getElementById(component).disabled = false;
+        }
+
+        // Get each component of the address from the place details
+        // and fill the corresponding field on the form.
+        for (var i = 0; i < place.address_components.length; i++) {
+            var addressType = place.address_components[i].types[0];
+            if (componentForm[addressType]) {
+            var val = place.address_components[i][componentForm[addressType]];
+            document.getElementById(addressType).value = val;
+            }
+        }
     }
 });
