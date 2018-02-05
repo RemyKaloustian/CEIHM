549057772f4b75c5ae943aaeaab6dd6b26b0c905
Kevin Duglué <kevin.duglue@gmail.com>
2018-02-05 08:41:35 +0100

    Ajout bouches d'incendies

--- js/locations.js
+++ js/locations.js
@@ -14,7 +14,7 @@ let locations = [{
     },
     {
         icon: 'assets/pics/feu.png',
-        address: '400 Avenue Roumanille, 06410 Biot',
+        address: '80 Rue Henri Point Carré',
         type: 1, //feu
     },
     {
@@ -32,5 +32,29 @@ let locations = [{
             lat: 43.620643,
             lng: 7.074611
         }
-    }
+    },
+    {
+        icon: 'assets/pics/eau.png',
+        type: 2, //bouche d'incendie
+        pos: {
+            lat: 43.618839,
+            lng: 7.076233
+        }
+    },
+    {
+        icon: 'assets/pics/eau-gris.png',
+        type: 3, //bouche d'incendie fermée
+        pos: {
+            lat: 43.619006,
+            lng: 7.075657
+        }
+    },
+    {
+        icon: 'assets/pics/eau.png',
+        type: 2, //bouche d'incendie
+        pos: {
+            lat: 43.618305,
+            lng: 7.076016
+        }
+    },
 ]
